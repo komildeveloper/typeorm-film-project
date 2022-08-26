e18dd99bf48732add28bf8e6ccc82bb09a9be79d
Komil Sobitov <64638545+komildeveloper@users.noreply.github.com>
2022-08-27 00:10:09 +0500

    added morgan

--- server.ts
+++ server.ts
@@ -1,6 +1,7 @@
 import express, { Application } from 'express'
 import swagger from 'swagger-ui-express'
 import dotenv from 'dotenv'
+import morgan from 'morgan'
 
 import router from './src/routes'
 import SwaggerDoc from './swagger.json'
@@ -11,6 +12,10 @@ const app: Application = express()
 
 app.use(express.json())
 app.use('/docs', swagger.serve, swagger.setup(SwaggerDoc))
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
 
 app.use('/', router)
 
