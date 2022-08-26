906eff30e66a8d384125bf06b20c025e4b666ace
Komil Sobitov <64638545+komildeveloper@users.noreply.github.com>
2022-08-26 23:41:31 +0500

    updated

--- dist/src/routes/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const actor_controller_1 = require("../controllers/actor.controller");
-const film_controller_1 = require("../controllers/film.controller");
-const router = (0, express_1.Router)();
-router.route('/actors').get(actor_controller_1.getActors).post(actor_controller_1.createActor, film_controller_1.createFilm);
-router.route('/actors/:id').put(actor_controller_1.updateActor).delete(actor_controller_1.deleteActor);
-exports.default = router;
